8275709e7b9dd093abeeb62d6958c0fc4981e354
shitalmarathe <shitalmarathe57@gmail.com>
2025-01-15 16:27:29 +0000

    move header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} antialiased`}>{children}</body>
+      <body className={`${outfit.className} antialiased`}>
+        <Header />
+        {children}
+      </body>
   </html>
   );
 }
